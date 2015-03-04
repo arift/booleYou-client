cd719fb882860eb1b9983a2927eeb6ca940b218d
michaelrabie <michaelrabie@gmail.com>
2015-03-04 15:18:36 -0500

    Added SignUp page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -25,4 +25,38 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
+})
+
+.controller('PopupCtrl',function($scope, $ionicPopup, $timeout) {
+
+ // Triggered on a button click, or some other target
+ $scope.showPopup = function() {
+   $scope.data = {}
+
+   // An elaborate, custom popup
+   var myPopup = $ionicPopup.show({
+     templateUrl: 'templates/signupBirthday.html',
+     title: 'Enter Birthday',
+     scope: $scope,
+     buttons: [
+       { text: 'Cancel' },
+       {
+         text: '<b>Save</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+           if (!$scope.data.wifi) {
+             //don't allow the user to close unless he enters wifi password
+             e.preventDefault();
+           } else {
+             return $scope.data.wifi;
+           }
+         }
+       },
+     ]
+   });
+   myPopup.then(function(res) {
+     console.log('Tapped!', res);
+   });
+
+  };
 });
---
+++ www/js/signupBirthday
@@ -0,0 +1,12 @@
+angular.module('booleYou', ['ionic'])
+.controller('PopupCtrl',function($scope, $ionicPopup, $timeout) {
+
+// Triggered on a button click, or some other target
+$scope.showPopup = function() {
+    console.log("here");
+
+  $scope.data = {}
+
+  // An elaborate, custom popup
+  
+ };
