2d1ba617b6a5f43e1138209bf23a7f9d201b7609
michaelrabie <michaelrabie@gmail.com>
2015-03-03 19:04:54 -0500

    Added a signup page and a sign up tab on the bottom. I plan on changing the location of the tab to a button on the login screen (once that is in existence)

--- www/js/app.js
+++ www/js/app.js
@@ -94,6 +94,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
+  })
+
+  .state('tab.signup', {
+    url: '/signup',
+    views: {
+      'tab-signup': {
+        templateUrl: 'templates/signup.html',
+        controller: 'AccountCtrl'
+      }
+    }
   });
 
   // if none of the above states are matched, use this as the fallback
