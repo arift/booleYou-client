97987192203cdc8e936a9d88f99017129abc2f6a
Ian Fox <ifox@umass.edu>
2015-03-08 19:11:27 -0400

    Massive Changes
    Consequences will never be the same

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+config.dbSettings = {};
+
+config.dbSettings.user_name = "booleuadmin";
+config.dbSettings.password = "b00leadmin";
+
+module.exports = config;
--- www/js/app.js
+++ www/js/app.js
@@ -107,6 +107,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/bitstream');
+  $urlRouterProvider.otherwise('/tab/signup');
 
 });
